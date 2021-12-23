66cc9c8990cb8256e74a73d98cbb7216adbc3528
Rustom Ichhaporia <45826892+rustom@users.noreply.github.com>
2021-12-23 06:23:42 -0600

    Add 404

---
+++ docs/component---src-pages-404-js-30c290fc1845641abd52.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhofstede_globe=self.webpackChunkhofstede_globe||[]).push([[883],{9616:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var u=t(7294),o=t(9692).default.div.withConfig({displayName:"sc-404__Background",componentId:"sc-qoauru-0"})([""]);function c(){return u.createElement(o,null)}}}]);
+//# sourceMappingURL=component---src-pages-404-js-30c290fc1845641abd52.js.map
---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Background = styled.div``;
+
+export default function FourOFour() {
+  return (
+    <Background>
+      
+    </Background>
+  );
+}
